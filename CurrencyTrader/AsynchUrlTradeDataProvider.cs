c6fbe0d8f39bcaeb81a05c491d38b4dd94fb1608
tgibbons-css <tgibbons@css.edu>
2016-11-10 11:02:27 -0600

    Partially implment asynch command

---
+++ CurrencyTrader/AsynchUrlTradeDataProvider.cs
@@ -0,0 +1,25 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader
+{
+    public class AsynchUrlTradeDataProvider : ITradeDataProvider
+    {
+        private readonly String url;
+        UrlTradeDataProvider SynchTradeProvider;
+        public AsynchUrlTradeDataProvider(String url)
+        {
+            this.url = url;
+            SynchTradeProvider = new UrlTradeDataProvider(url);
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            Task.Run(() => SynchTradeProvider.GetTradeData());
+            //return SynchTradeProvider.GetTradeData();
+        }
+    }
+}
